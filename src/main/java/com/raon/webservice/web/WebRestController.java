7c06e2a8af0d621a1952e5c8e9cd6852375ffca1
한성준 <hsjpmh@naver.com>
2018-11-27 16:20:43 +0900

    # h2.console.enabled = true # method posts add

--- src/main/java/com/raon/webservice/web/WebRestController.java
+++ src/main/java/com/raon/webservice/web/WebRestController.java
@@ -1,9 +1,12 @@
 package com.raon.webservice.web;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.raon.webservice.web.domain.posts.Posts;
 import com.raon.webservice.web.domain.posts.PostsInsertDto;
 import com.raon.webservice.web.domain.posts.PostsRepository;
 
@@ -20,9 +23,14 @@ public class WebRestController {
 		return " Hello! World ! ";
 	}
 	
-	@PostMapping("/posts")
+	@PostMapping("/savePosts")
 	public void insertPosts(PostsInsertDto dto) {
 		postsRepository.save(dto.toEntity());
+	}
+	
+	@GetMapping("/posts")
+	public List<Posts> posts() {
+		return postsRepository.findAll();
 	}
 	
 }
